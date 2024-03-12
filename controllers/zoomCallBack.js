fd6640e941c92dedde668a446f70eed653ccf3d4
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-03-12 23:54:18 +0200

    Enable zoom webhook

--- controllers/zoomCallBack.js
+++ controllers/zoomCallBack.js
@@ -1,14 +1,25 @@
 const factory = require("./controllersFactory");
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 const coursesModel = require("../models/coursesModel");
 const classModel = require("../models/classModel");
 const userModel = require("../models/userModel");
 const ApiError = require("../utils/ApiError");
 
-exports.getCallBack= asyncHandler(async (req, res, next) => {
-    console.log(req.body)
+exports.getCallBack = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
 
-    res.status(200).json({message:"message"});
+  // Webhook request event type is a challenge-response check
+  if (req.body.event === "endpoint.url_validation") {
+    const hashForValidate = crypto
+      .createHmac("sha256", "KZNnqA3tTzq9ZZOLwgFwSw")
+      .update(request.body.payload.plainToken)
+      .digest("hex");
+
+    res.status(200).json({
+      plainToken: request.body.payload.plainToken,
+      encryptedToken: hashForValidate,
+    });
+  }
 });
