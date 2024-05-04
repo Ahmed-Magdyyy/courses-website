5bb712e516ea913005469fa98aa00b290c04f127
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-05-04 17:31:29 +0300

    b

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -140,7 +140,21 @@ exports.getUser = asyncHandler(async (req, res, next) => {
             name: 1,
             email: 1,
             phone: 1,
-            role:1,
+            role: 1,
+            password: 1,
+            passwordChangedAT: 1,
+            passwordResetCode: 1,
+            passwordResetCodeExpire: 1,
+            passwordResetCodeVerified: 1,
+            enabledControls: 1,
+            account_status: 1,
+            active: 1,
+            courses: 1,
+            classes: 1,
+            products: 1,
+            remainingClasses: 1,
+            createdAt: 1,
+            updatedAt: 1,
             classes: {
               $map: {
                 input: "$classes",
@@ -152,8 +166,7 @@ exports.getUser = asyncHandler(async (req, res, next) => {
                   status: "$$class.status",
                 },
               },
-            },
-          },
+            },          },
         },
         {
           $addFields: {
