033932bac325e7db89f43a732de355f459fdb831
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-05-16 02:50:00 +0300

    m

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 const morgan = require("morgan");
 const cors = require("cors");
 const path = require("path");
+const https = require('https');
 
 const ApiError = require("./utils/ApiError");
 const globalError = require("./middlewares/errorMiddleware");
@@ -70,24 +71,27 @@ const server = app.listen(process.env.PORT, () =>
 // Initialize Socket.IO
 socketConfig.initSocketServer(server);
 
-// // Ping the server immediately after starting the server
-// pingServer();
+// ------------------------------------------------------------------------------------------
+// Ping the server immediately after starting the server
+pingServer();
 
-// // Ping the server every 14 minutes (14 * 60 * 1000 milliseconds)
-// const pingInterval = 14 * 60 * 1000;
-// setInterval(pingServer, pingInterval);
+// Ping the server every 14 minutes (14 * 60 * 1000 milliseconds)
+const pingInterval = 14 * 60 * 1000;
+setInterval(pingServer, pingInterval);
 
-// // Function to ping the server by hitting the specified API route
-// function pingServer() {
-//   const pingEndpoint = 'https://samaco-summer.onrender.com/api/v1/auth/login';
+// Function to ping the server by hitting the specified API route
+function pingServer() {
+  const pingEndpoint = 'https://courses-website-t4cr.onrender.com/api/v1/auth/login';
 
-//   // Send a GET request to the ping endpoint
-//   https.get(pingEndpoint, (res) => {
-//     console.log(`Ping sent to server: ${res.statusCode}`);
-//   }).on('error', (err) => {
-//     console.error('Error while sending ping:', err);
-//   });
-// }
+  // Send a GET request to the ping endpoint
+  https.get(pingEndpoint, (res) => {
+    console.log(`Ping sent to server: ${res.statusCode}`);
+  }).on('error', (err) => {
+    console.error('Error while sending ping:', err);
+  });
+}
+// ------------------------------------------------------------------------------------------
+
 
 // UnhandledRejections event handler (rejection outside express)
 process.on("unhandledRejection", (err) => {
