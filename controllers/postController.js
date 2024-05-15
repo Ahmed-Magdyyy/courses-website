4046e6c8fc086070d701b309b911987b9a90dd69
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-05-16 01:19:03 +0300

    n

--- controllers/postController.js
+++ controllers/postController.js
@@ -190,7 +190,7 @@ exports.editPost = asyncHandler(async (req, res, next) => {
     console.log("====================================");
     const olddata = oldMedia;
     console.log("TYPE:", Array.isArray(oldMedia));
-    console.log("OLD MEDIA 22:", olddata);
+    console.log(olddata);
     console.log(
       "OLD MEDIA 33:",
       oldMedia.forEach((media) => console.log(media))
