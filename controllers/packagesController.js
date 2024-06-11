dec96501cd8712fd8de804e101603db8d8a42bcb
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-06-11 11:06:58 +0300

    adding packages and packages subscripbtion

--- controllers/packagesController.js
+++ controllers/packagesController.js
@@ -52,131 +52,6 @@ exports.getPackages = asyncHandler(async (req, res, next) => {
   res.status(200).json({ message: "Success", data: packages });
 });
 
-// exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
-//   const { packageId } = req.params;
-
-//   const user = await User.findById(req.user._id);
-
-//   if (!user) {
-//     return next(new ApiError(`No user found`, 400));
-//   }
-
-//   const selectedPackage = await Package.findById(packageId);
-
-//   if (!selectedPackage) {
-//     return next(new ApiError(`No package found`, 400));
-//   }
-//   const stripeProduct = await stripe.products.retrieve(
-//     selectedPackage.packageStripeId
-//   );
-
-//   const prices = await stripe.prices.list({
-//     product: "prod_QGZw9iFJEhfZh0",
-//   });
-// // const yoo =prices.data.filter((price) => price.lookup_key== "starter_aed")[0];
-//   console.log("====================================");
-//   console.log("prices:", prices.data[0]);
-//   console.log("====================================");
-
-//   const price = await stripe.prices.retrieve(stripeProduct.default_price);
-//   console.log("====================================");
-//   console.log("stripeProduct:", stripeProduct);
-//   console.log("====================================");
-//   console.log("====================================");
-//   console.log("price:", price);
-//   console.log("selectedPackage:", selectedPackage);
-//   console.log("====================================");
-
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         price: stripeProduct.default_price,
-//         quantity: 1,
-//       },
-//     ],
-//     mode: "payment",
-//     success_url: `${req.protocol}://${req.get("host")}/purchases`,
-//     cancel_url: `${req.protocol}://${req.get("host")}/packages`,
-//     customer_email: user.email,
-//     client_reference_id: selectedPackage._id.toString(),
-//     metadata: {
-//       userId: user._id.toString(),
-//       packageId: selectedPackage._id.toString(),
-//       stripePackageId: selectedPackage.packageStripeId,
-//     },
-//   });
-
-//   res.status(200).json({ success: true, session });
-// });
-
-// exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
-//   const { packageId } = req.params;
-//   const { currency } = req.query;
-
-//   const user = await User.findById(req.user._id);
-
-//   if (!user) {
-//     return next(new ApiError(`No user found`, 400));
-//   }
-
-//   const subscribedUser = await stripe.customers.list({
-//     email:user.email,
-//     limit: 1
-//   })
-
-//   console.log("====================================");
-//   console.log("subscribedUser:", subscribedUser);
-//   console.log("====================================");
-
-//   const existSubscribtion = await stripe.subscriptions.list({
-//     customer:subscribedUser.data[0].id,
-//     // status: 'active',
-//     // limit: 1
-//   })
-
-//   console.log("====================================");
-//   console.log("existSubscribtion:", existSubscribtion);
-//   console.log("====================================");
-
-//   const selectedPackage = await Package.findById(packsageId);
-
-//   if (!selectedPackage) {
-//     return next(new ApiError(`No package found`, 400));
-//   }
-
-//   // Find the price in the selected package that matches the requested currency
-//   const selectedPrice = selectedPackage.prices.find(price => price.currency === currency.toLowerCase());
-
-//   if (!selectedPrice) {
-//     return next(new ApiError(`No price found for the specified currency`, 400));
-//   }
-
-//   console.log("====================================");
-//   console.log("selectedPrice:", selectedPrice);
-//   console.log("====================================");
-
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         price: selectedPrice.stripePriceId,
-//         quantity: 1,
-//       },
-//     ],
-//     mode: "subscription", // Assuming a subscription model
-//     success_url: `${req.protocol}://${req.get("host")}/purchases`,
-//     cancel_url: `${req.protocol}://${req.get("host")}/packages`,
-//     customer_email: user.email,
-//     client_reference_id: selectedPackage._id.toString(),
-//     metadata: {
-//       userId: user._id.toString(),
-//       packageId: selectedPackage._id.toString(),
-//       stripePackageId: selectedPackage.packageStripeId,
-//     },
-//   });
-
-//   res.status(200).json({ success: true, session });
-// });
-
 exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
   const { packageId } = req.params;
   const { currency } = req.query;
@@ -197,18 +72,10 @@ exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
     selectedPackage.packageStripeId
   );
 
-  console.log("====================================");
-  console.log("stripeProduct", stripeProduct);
-  console.log("====================================");
-
-  console.log("////////////////////////////////////");
   const selectedPrice = selectedPackage.prices.find(
     (price) => price.currency === currency.toLowerCase()
   );
 
-  console.log("====================================");
-  console.log("selectedPackage", selectedPrice.stripePriceId);
-  console.log("====================================");
   const priceId = selectedPrice.stripePriceId;
 
   // Check if the user already exists in Stripe
@@ -218,10 +85,6 @@ exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
     stripeCustomer = customers.data[0];
   }
 
-  console.log("====================================");
-  console.log("stripeCustomer", stripeCustomer);
-  console.log("====================================");
-
   // If the user exists, check for active subscriptions
   if (stripeCustomer) {
     const subscriptions = await stripe.subscriptions.list({
@@ -229,10 +92,6 @@ exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
       status: "active",
       expand: ["data.default_payment_method"],
     });
-
-    console.log("====================================");
-    console.log("subscriptions", subscriptions.data);
-    console.log("====================================");
 
     if (subscriptions.data.length > 0) {
       // Redirect to the customer portal to manage the subscription
@@ -254,9 +113,7 @@ exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
     user.subscription.stripeCustomerId = stripeCustomer.id;
     await user.save();
   }
-  console.log("====================================");
-  console.log("priceId", priceId);
-  console.log("====================================");
+
   // Create a new checkout session
   const session = await stripe.checkout.sessions.create({
     mode: "subscription",
