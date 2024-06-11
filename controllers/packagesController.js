5f5aa7fc24710cf7ab2523b3c56bf3ecdf0e4fef
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-06-11 14:53:53 +0300

    adding webhook for stripe

--- controllers/packagesController.js
+++ controllers/packagesController.js
@@ -183,6 +183,10 @@ async function handleSubscriptionCreated(session, subscription) {
   console.log('====================================');
   const userId = session.metadata.userId;
   const user = await User.findById(userId);
+  console.log('====================================');
+  console.log("user.remainingClasses:",user.remainingClasses);
+  console.log("session.metadata.classesNum:",session.metadata.classesNum);
+  console.log('====================================');
   if (user) {
     if (user.role=== "student") user.remainingClasses =user.remainingClasses + session.metadata.classesNum
     user.subscribed = true;
