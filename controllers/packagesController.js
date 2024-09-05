2ced104bf6cdcc7cb06c36d8e5db2c4c76bfa3ec
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-09-05 05:09:09 +0300

    testing

--- controllers/packagesController.js
+++ controllers/packagesController.js
@@ -223,6 +223,14 @@ exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
     await user.save();
   }
 
+  const metadata = {
+    system: "jawwid",
+    userId: user._id.toString(),
+    packageId: selectedPackage._id.toString(),
+    stripePackageId: selectedPackage.packageStripeId,
+    classesNum: selectedPackage.classesNum,
+  };
+
   // Create a new checkout session
   const session = await stripe.checkout.sessions.create({
     mode: "subscription",
@@ -236,12 +244,9 @@ exports.createCheckoutSession = asyncHandler(async (req, res, next) => {
     cancel_url: `https://learning.jawwid.com/subscriptions/packages`,
     customer: stripeCustomer.id,
     client_reference_id: selectedPackage._id.toString(),
-    metadata: {
-      system: "jawwid",
-      userId: user._id.toString(),
-      packageId: selectedPackage._id.toString(),
-      stripePackageId: selectedPackage.packageStripeId,
-      classesNum: selectedPackage.classesNum,
+    metadata,
+    payment_intent_data: {
+      metadata,
     },
   });
 
@@ -295,13 +300,13 @@ exports.createOneTimePaymentSession = asyncHandler(async (req, res, next) => {
     await user.save();
   }
 
-const metadata = {
-  system: "jawwid",
-  userId: user._id.toString(),
-  packageId: selectedPackage._id.toString(),
-  stripePackageId: selectedPackage.packageStripeId,
-  classesNum: selectedPackage.classesNum,
-}
+  const metadata = {
+    system: "jawwid",
+    userId: user._id.toString(),
+    packageId: selectedPackage._id.toString(),
+    stripePackageId: selectedPackage.packageStripeId,
+    classesNum: selectedPackage.classesNum,
+  };
 
   // Create a new one-time payment session
   const session = await stripe.checkout.sessions.create({
@@ -318,7 +323,6 @@ const metadata = {
     customer: stripeCustomer.id,
     client_reference_id: selectedPackage._id.toString(),
     metadata,
-
     payment_intent_data: {
       metadata,
     },
@@ -667,67 +671,67 @@ exports.managePackageSubscription = asyncHandler(async (req, res, next) => {
   res.status(200).json({ url: session.url });
 });
 
-// exports.getAllPaidInvoices = asyncHandler(async (req, res, next) => {
-//   try {
-//     // Destructure page and limit from request params with default values
-//     const { page = 1, limit = 10 } = req.query;
+exports.getAllPaidInvoices = asyncHandler(async (req, res, next) => {
+  try {
+    // Destructure page and limit from request params with default values
+    const { page = 1, limit = 10 } = req.query;
 
-//     // Validate page and limit parameters (optional, for extra security)
-//     if (page < 1 || limit < 1 || limit > 100) {
-//       return res
-//         .status(400)
-//         .json({ message: "Invalid page or limit parameters" });
-//     }
+    // Validate page and limit parameters (optional, for extra security)
+    if (page < 1 || limit < 1 || limit > 100) {
+      return res
+        .status(400)
+        .json({ message: "Invalid page or limit parameters" });
+    }
 
-//     const startingAfter = req.query.starting_after; // Optional cursor for pagination
-//     const endingBefore = req.query.ending_before; // Optional cursor for pagination (mutually exclusive with startingAfter)
+    const startingAfter = req.query.starting_after; // Optional cursor for pagination
+    const endingBefore = req.query.ending_before; // Optional cursor for pagination (mutually exclusive with startingAfter)
 
-//     const stripeParams = {
-//       status: "paid",
-//       limit: Math.min(limit, 100), // Enforce maximum limit of 100 for security
-//     };
+    const stripeParams = {
+      status: "paid",
+      limit: Math.min(limit, 100), // Enforce maximum limit of 100 for security
+    };
 
-//     // Use startingAfter or endingBefore for pagination if provided
-//     if (startingAfter) {
-//       stripeParams.starting_after = startingAfter;
-//     } else if (endingBefore) {
-//       stripeParams.ending_before = endingBefore;
-//     }
+    // Use startingAfter or endingBefore for pagination if provided
+    if (startingAfter) {
+      stripeParams.starting_after = startingAfter;
+    } else if (endingBefore) {
+      stripeParams.ending_before = endingBefore;
+    }
 
-//     const invoices = await stripe.invoices.list(stripeParams);
+    const invoices = await stripe.invoices.list(stripeParams);
 
-//     // Transform invoices to desired format
-//     const paidInvoices = invoices.data.map((invoice) => ({
-//       invoiceId: invoice.id,
-//       invoice_number: invoice.number,
-//       customer_name: invoice.customer_name || "N/A", // Fallback if customer_name is not available
-//       customer_email: invoice.customer_email,
-//       package_name: invoice.lines.data[0].description.split("× ")[1],
-//       amount_paid: invoice.amount_paid / 100,
-//       currency: invoice.currency.toUpperCase(),
-//       subscription_start: new Date(invoice.lines.data[0].period.start * 1000),
-//       subscription_end: new Date(invoice.lines.data[0].period.end * 1000),
-//       invoice_url: invoice.hosted_invoice_url,
-//       invoice_pdf: invoice.invoice_pdf,
-//       created_at: new Date(invoice.created * 1000),
-//     }));
+    // Transform invoices to desired format
+    const paidInvoices = invoices.data.map((invoice) => ({
+      invoiceId: invoice.id,
+      invoice_number: invoice.number,
+      customer_name: invoice.customer_name || "N/A", // Fallback if customer_name is not available
+      customer_email: invoice.customer_email,
+      package_name: invoice.lines.data[0].description.split("× ")[1],
+      amount_paid: invoice.amount_paid / 100,
+      currency: invoice.currency.toUpperCase(),
+      subscription_start: new Date(invoice.lines.data[0].period.start * 1000),
+      subscription_end: new Date(invoice.lines.data[0].period.end * 1000),
+      invoice_url: invoice.hosted_invoice_url,
+      invoice_pdf: invoice.invoice_pdf,
+      created_at: new Date(invoice.created * 1000),
+    }));
 
-//     // const hasNextPage = invoices.has_more; // Check for next page based on Stripe response
-//     // const nextStartingAfter = invoices.data[invoices.data.length - 1].id; // Get next page cursor (starting_after)
+    // const hasNextPage = invoices.has_more; // Check for next page based on Stripe response
+    // const nextStartingAfter = invoices.data[invoices.data.length - 1].id; // Get next page cursor (starting_after)
 
-//     res.status(200).json({
-//       message: "Success",
-//       data: paidInvoices,
-//       // pagination: {
-//       //   hasNextPage,
-//       //   nextStartingAfter, // Include next page cursor for client-side pagination
-//       // },
-//     });
-//   } catch (error) {
-//     console.error("Error fetching invoices:", error);
-//     res.status(500).json({ message: "Error fetching invoices", error });
-//   }
-// });
+    res.status(200).json({
+      message: "Success",
+      data: paidInvoices,
+      // pagination: {
+      //   hasNextPage,
+      //   nextStartingAfter, // Include next page cursor for client-side pagination
+      // },
+    });
+  } catch (error) {
+    console.error("Error fetching invoices:", error);
+    res.status(500).json({ message: "Error fetching invoices", error });
+  }
+});
 
 exports.getStudentInvoice = asyncHandler(async (req, res, next) => {
   if (req.user.subscription.stripeCustomerId !== null) {
@@ -845,63 +849,24 @@ exports.getBankTransfer = asyncHandler(async (req, res, next) => {
 
 // exports.getAllPaidInvoices = asyncHandler(async (req, res, next) => {
 //   try {
-//     // Fetch all paid invoices (both subscription and one-time payment)
-//     const invoices = await stripe.invoices.list({
-//       status: "paid",
-//       limit: 100, // Set a high limit to ensure fetching as many invoices as possible in one call
-//     });
-
-//     const paidInvoices = invoices.data.map((invoice) => {
-//       const isSubscription = !!invoice.subscription; // Check if it's a subscription invoice
-
-//       return {
-//         invoiceId: invoice.id,
-//         invoice_number: invoice.number,
-//         customer_name: invoice.customer_name || "N/A",
-//         customer_email: invoice.customer_email,
-//         package_name: isSubscription
-//           ? invoice.lines.data[0].description.split("× ")[1]
-//           : invoice.lines.data[0].description,
-//         amount_paid: invoice.amount_paid / 100,
-//         currency: invoice.currency.toUpperCase(),
-//         subscription_start: isSubscription
-//           ? new Date(invoice.lines.data[0].period.start * 1000)
-//           : null,
-//         subscription_end: isSubscription
-//           ? new Date(invoice.lines.data[0].period.end * 1000)
-//           : null,
-//         invoice_url: invoice.hosted_invoice_url,
-//         invoice_pdf: invoice.invoice_pdf,
-//         created_at: new Date(invoice.created * 1000),
-//       };
-//     });
-
-//     res.status(200).json({
-//       message: "Success",
-//       data: paidInvoices,
-//     });
-//   } catch (error) {
-//     console.error("Error fetching invoices:", error);
-//     res.status(500).json({ message: "Error fetching invoices", error });
-//   }
-// });
-
-// exports.getAllPaidInvoices = asyncHandler(async (req, res, next) => {
-//   try {
-//     // Fetch all paid charges (one-time payments)
+//     // Fetch all paid charges
 //     const charges = await stripe.charges.list({
-//       limit: 100,
-//       paid: true,
+//       limit: 100, // Adjust the limit as necessary
+//       paid: true, // Only include charges that have been successfully paid
+//       expand: ["data.invoice"],
 //     });
 
-//     console.log(charges)
+//     // Filter to include only one-time payments related to your new system
+//     const oneTimePayments = charges.data.filter((charge) => {
+//       return charge.metadata.system === "jawwid" && !charge.invoice;
+//     });
 
-//     const paidCharges = charges.data.map((charge) => ({
+//     const formattedCharges = oneTimePayments.map((charge) => ({
 //       chargeId: charge.id,
 //       customer_name: charge.billing_details.name || "N/A",
 //       customer_email: charge.billing_details.email || "N/A",
 //       description: charge.description || "One-time payment",
-//       amount_paid: charge.amount / 100,
+//       amount_paid: charge.amount / 100, // Convert from cents to currency
 //       currency: charge.currency.toUpperCase(),
 //       created_at: new Date(charge.created * 1000),
 //       receipt_url: charge.receipt_url,
@@ -909,91 +874,10 @@ exports.getBankTransfer = asyncHandler(async (req, res, next) => {
 
 //     res.status(200).json({
 //       message: "Success",
-//       data: paidCharges,
+//       data: formattedCharges,
 //     });
 //   } catch (error) {
 //     console.error("Error fetching charges:", error);
-//     res.status(500).json({ message: "Error fetching charges", error });
+//     res.status(500).json({ message: "Error fetching payments", error });
 //   }
 // });
-
-// exports.getStudentInvoice = asyncHandler(async (req, res, next) => {
-//   try {
-//     if (req.user.subscription.stripeCustomerId !== null) {
-//       const invoices = await stripe.invoices.list({
-//         customer: req.user.subscription.stripeCustomerId,
-//         limit: 100, // Set a high limit to ensure fetching as many invoices as possible in one call
-//       });
-
-//       const studentInvoices = invoices.data.map((invoice) => {
-//         const isSubscription = !!invoice.subscription;
-
-//         return {
-//           invoiceId: invoice.id,
-//           status: invoice.status,
-//           invoice_number: invoice.number,
-//           customer_name: invoice.customer_name || "N/A",
-//           customer_email: invoice.customer_email,
-//           package_name: isSubscription
-//             ? invoice.lines.data[0].description.split("× ")[1]
-//             : invoice.lines.data[0].description,
-//           amount_paid: invoice.amount_paid / 100,
-//           currency: invoice.currency.toUpperCase(),
-//           subscription_start: isSubscription
-//             ? new Date(invoice.lines.data[0].period.start * 1000)
-//             : null,
-//           subscription_end: isSubscription
-//             ? new Date(invoice.lines.data[0].period.end * 1000)
-//             : null,
-//           invoice_url: invoice.hosted_invoice_url,
-//           invoice_pdf: invoice.invoice_pdf,
-//           created_at: new Date(invoice.created * 1000),
-//         };
-//       });
-
-//       res.status(200).json(studentInvoices);
-//     } else {
-//       res.status(200).json({ message: "No invoices" });
-//     }
-//   } catch (error) {
-//     console.error("Error fetching student invoices:", error);
-//     res.status(500).json({ message: "Error fetching student invoices", error });
-//   }
-// });
-
-exports.getAllPaidInvoices = asyncHandler(async (req, res, next) => {
-  try {
-    // Fetch all paid charges
-    const charges = await stripe.charges.list({
-      limit: 100, // Adjust the limit as necessary
-      paid: true, // Only include charges that have been successfully paid
-      expand: ["data.invoice"],
-    });
-
-    console.log("charges:::::::::::", charges);
-
-    // Filter to include only one-time payments related to your new system
-    const oneTimePayments = charges.data.filter((charge) => {
-      return charge.metadata.system === "jawwid" && !charge.invoice;
-    });
-
-    const formattedCharges = oneTimePayments.map((charge) => ({
-      chargeId: charge.id,
-      customer_name: charge.billing_details.name || "N/A",
-      customer_email: charge.billing_details.email || "N/A",
-      description: charge.description || "One-time payment",
-      amount_paid: charge.amount / 100, // Convert from cents to currency
-      currency: charge.currency.toUpperCase(),
-      created_at: new Date(charge.created * 1000),
-      receipt_url: charge.receipt_url,
-    }));
-
-    res.status(200).json({
-      message: "Success",
-      data: formattedCharges,
-    });
-  } catch (error) {
-    console.error("Error fetching charges:", error);
-    res.status(500).json({ message: "Error fetching payments", error });
-  }
-});
