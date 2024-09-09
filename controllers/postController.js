d10ce1c53309cee4b2d9594de8699b72a38ba258
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-09-09 05:17:47 +0300

    testing

--- controllers/postController.js
+++ controllers/postController.js
@@ -120,7 +120,7 @@ exports.createPost = asyncHandler(async (req, res, next) => {
   const { content, url, visibleTo } = req.body;
   const media = req.body.media || [];
 
-  const allowedVisibleToValues = ["student", "teacher", "admin", "guest"];
+  const allowedVisibleToValues = ["student", "teacher", "admin"];
 
   // Validate visibleTo field
   if (visibleTo && !Array.isArray(visibleTo)) {
@@ -132,7 +132,7 @@ exports.createPost = asyncHandler(async (req, res, next) => {
   }
 
   // Default to all roles if visibleTo is not provided
-  let postVisibleTo = ["student", "teacher", "admin", "guest"]; //Default value
+  let postVisibleTo = ["student", "teacher", "admin"]; //Default value
 
   if (visibleTo && visibleTo.length > 0) {
     postVisibleTo = visibleTo;
@@ -764,7 +764,7 @@ exports.changePostVisibleTo = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const { visibleTo } = req.body;
 
-  const allowedVisibleToValues = ["student", "teacher", "admin", "guest"];
+  const allowedVisibleToValues = ["student", "teacher", "admin"];
   if (
     !Array.isArray(visibleTo) ||
     visibleTo.some((role) => !allowedVisibleToValues.includes(role))
